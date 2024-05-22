46ad5d4167b0e882b6c147f0825d5372ece5dfc2
carlaaotero <carla.otero@estudiantat.upc.edu>
2024-05-22 20:13:27 +0200

    Sisena versió

--- app/src/main/java/com/example/client_dsa/Classes/LoginComp.java
+++ app/src/main/java/com/example/client_dsa/Classes/LoginComp.java
@@ -1,17 +1,17 @@
 package com.example.client_dsa.Classes;
 
 public class LoginComp {
-    private String username;
+    private String nomusuari;
     private String password;
-    public LoginComp (String username, String password){
-        this.username=username;
+    public LoginComp (String nomusuari, String password){
+        this.nomusuari=nomusuari;
         this.password=password;
     }
     public String getUsername() {
-        return username;
+        return nomusuari;
     }
-    public void setUsername(String username) {
-        this.username = username;
+    public void setUsername(String nomusuari) {
+        this.nomusuari = nomusuari;
     }
     public String getPassword() {
         return password;
@@ -20,6 +20,6 @@ public class LoginComp {
         this.password = password;
     }
     public String toString(){
-        return username;
+        return nomusuari;
     }
 }
